2fbe24ce2c169b5489a43a5d564dfecd995533bc
Oliver Veits @ vocon IT GmbH <oliver.veits@vocon-it.com>
2022-06-25 13:42:46 +0000

    Full machine learning example

---
+++ install.sh
@@ -0,0 +1,10 @@
+
+
+python -m pip install --upgrade pip
+python -m pip install scipy numpy matplotlib pandas sklearn
+
+# Optional: for successfully showing graphics (you can save and view the plots within pycharm IDE instead)
+apt-get update && apt-get install -y python3-tk
+
+
+
