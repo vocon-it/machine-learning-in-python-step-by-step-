1f14d22c8c0195153d425755692a4c5282d73642
Oliver Veits @ gmail <oliver.veits@gmail.com>
2023-04-17 17:13:48 +0000

    fixed install.sh and fixed identation in main.py

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
+set -e
 
-
-python -m pip install --upgrade pip
-python -m pip install scipy numpy matplotlib pandas sklearn
+pip3 install --upgrade pip
+pip3 install -U scikit-learn scipy numpy matplotlib pandas sklearn
 
 # Optional: for successfully showing graphics (you can save and view the plots within pycharm IDE instead)
 apt-get update && apt-get install -y python3-tk
--- main.py
+++ main.py
@@ -15,152 +15,145 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-    #
-    # Machine Learning Hello World
-    #
-    #   Example from https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
-    #
-    #   Before running the script, please run
-    #       bash install.sh
-    #   in a terminal
+#
+# Machine Learning Hello World
+#
+#   Example from https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+#
+#   Before running the script, please run
+#       bash install.sh
+#   in a terminal
 
-    # Python version
-    import sys
+# Python version
+import sys
 
-    print('Python: {}'.format(sys.version))
-    # scipy
-    import scipy
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
 
-    print('scipy: {}'.format(scipy.__version__))
-    # numpy
-    import numpy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy
 
-    print('numpy: {}'.format(numpy.__version__))
-    # matplotlib
-    import matplotlib
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
 
-    print('matplotlib: {}'.format(matplotlib.__version__))
-    # pandas
-    import pandas
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
 
-    print('pandas: {}'.format(pandas.__version__))
-    # scikit-learn
-    import sklearn
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
 
-    print('sklearn: {}'.format(sklearn.__version__))
+print('sklearn: {}'.format(sklearn.__version__))
 
-    # Load libraries
-    from pandas import read_csv
-    from pandas.plotting import scatter_matrix
-    from matplotlib import pyplot
-    from sklearn.model_selection import train_test_split
-    from sklearn.model_selection import cross_val_score
-    from sklearn.model_selection import StratifiedKFold
-    from sklearn.metrics import classification_report
-    from sklearn.metrics import confusion_matrix
-    from sklearn.metrics import accuracy_score
-    from sklearn.linear_model import LogisticRegression
-    from sklearn.tree import DecisionTreeClassifier
-    from sklearn.neighbors import KNeighborsClassifier
-    from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-    from sklearn.naive_bayes import GaussianNB
-    from sklearn.svm import SVC
+# Load libraries
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 
-    # ...
+# ...
 
-    # Load dataset
-    url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
-    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-    dataset = read_csv(url, names=names)
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
 
-    # shape
-    print(dataset.shape)
+# shape
+print(dataset.shape)
 
-    # head
-    print(dataset.head(20))
+# head
+print(dataset.head(20))
 
-    # descriptions
-    print(dataset.describe())
+# descriptions
+print(dataset.describe())
 
-    # class distribution
-    print(dataset.groupby('class').size())
+# class distribution
+print(dataset.groupby('class').size())
 
-    #
-    # 4 Visualization
-    #
+#
+# 4 Visualization
+#
 
-    # box and whisker plots
-    pyplot.clf()
-    dataset.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
-    pyplot.savefig("box-and-whisker.png")
-    # pyplot.show()
+# box and whisker plots
+pyplot.clf()
+dataset.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
+pyplot.savefig("box-and-whisker.png")
+# pyplot.show()
 
-    ...
-    # histograms
-    pyplot.clf()
-    dataset.hist()
-    pyplot.savefig("histograms.png")
-    # pyplot.show()
+...
+# histograms
+pyplot.clf()
+dataset.hist()
+pyplot.savefig("histograms.png")
+# pyplot.show()
 
-    ...
-    # scatter plot matrix
-    pyplot.clf()
-    scatter_matrix(dataset)
-    pyplot.savefig("scatter_matrix.png")
-    # pyplot.show()
+...
+# scatter plot matrix
+pyplot.clf()
+scatter_matrix(dataset)
+pyplot.savefig("scatter_matrix.png")
+# pyplot.show()
 
-    ...
-    # Split-out validation dataset
-    array = dataset.values
-    X = array[:, 0:4]
-    y = array[:, 4]
-    X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+...
+# Split-out validation dataset
+array = dataset.values
+X = array[:, 0:4]
+y = array[:, 4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
 
-    ...
-    # Spot Check Algorithms
-    models = []
-    models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
-    models.append(('LDA', LinearDiscriminantAnalysis()))
-    models.append(('KNN', KNeighborsClassifier()))
-    models.append(('CART', DecisionTreeClassifier()))
-    models.append(('NB', GaussianNB()))
-    models.append(('SVM', SVC(gamma='auto')))
-    # evaluate each model in turn
-    results = []
-    names = []
-    for name, model in models:
-        kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-        cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
-        results.append(cv_results)
-        names.append(name)
-        print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+...
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
 
-    ...
-    # Compare Algorithms
-    pyplot.clf()
-    pyplot.boxplot(results, labels=names)
-    pyplot.title('Algorithm Comparison')
-    pyplot.savefig("compare-algorithms.png")
-    # pyplot.show()
+...
+# Compare Algorithms
+pyplot.clf()
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.savefig("compare-algorithms.png")
+# pyplot.show()
 
-    ...
-    # Make predictions on validation dataset
-    model = SVC(gamma='auto')
-    model.fit(X_train, Y_train)
-    predictions = model.predict(X_validation)
+...
+# Make predictions on validation dataset
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
 
-    ...
-    # Evaluate predictions
-    print(accuracy_score(Y_validation, predictions))
-    print(confusion_matrix(Y_validation, predictions))
-    print(classification_report(Y_validation, predictions))
+...
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
 
 
-    # # import tkinter
-    # import matplotlib
-    # import matplotlib.pyplot as plt
-    #
-    # matplotlib.use('TkAgg')
-    # plt.plot([1, 2, 3], [5, 7, 4])
-    # plt.show()
 
